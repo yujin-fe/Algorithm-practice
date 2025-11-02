84d2ac6bcdc618c1b649c78ae0c2a71bae5aed76
yujin-fe <hyujin1224@gmail.com>
2025-11-02 18:31:44 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,14 +1,4 @@
 function solution(numbers, k) {
-    if(numbers.length%2===0){
-        const odd = numbers.filter((num)=>num%2!==0)
-        const turn = k%odd.length
-        return turn===0?odd[odd.length-1]:odd[turn-1]
-    }else{
-        const odd = numbers.slice().filter((num)=>num%2!==0)
-        const even = numbers.slice().filter((num)=>num%2===0)
-        const numArr = [...odd,...even]
-        const turn = k%numArr.length
-        return turn===0?numArr[numArr.length-1]:numArr[turn-1]
-    }
-    
+
+    return numbers[((2 * (k -1))) % numbers.length];
 }
