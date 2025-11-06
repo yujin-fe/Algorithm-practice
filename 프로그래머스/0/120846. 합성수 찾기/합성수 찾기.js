35f0f56739d7b4a76d51f4d968f98cd09c69ddfd
yujin-fe <hyujin1224@gmail.com>
2025-11-06 17:14:04 +0900

    [level 0] Title: 합성수 찾기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,18 +1,18 @@
 function solution(n) {
     if(n<3) return 0
-    let prime_count = 0
+    let count = 0
     for(i=4; i<=n; i++){
         if((
-            i%2!==0 && 
-            i%3!==0 && 
-            i%5!==0 && 
-            i%7!==0
-        ) || 
-           i===5 ||
-           i===7
+            i%2===0 ||
+            i%3===0 || 
+            i%5===0 || 
+            i%7===0
+        ) && 
+           i!==5 &&
+           i!==7
           ){
-            prime_count++
+            count++
         }
     }
-    return n-prime_count-3
+    return count
 }
